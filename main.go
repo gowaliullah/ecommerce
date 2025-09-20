29b452fd4e65ba1757f6f0ad07ff60bd759faafe
MD Waliullah <waliullah.godev@gmail.com>
2025-09-20 08:36:35 +0600

    product type created

--- main.go
+++ main.go
@@ -5,6 +5,14 @@ import (
 	"net/http"
 )
 
+type Product struct {
+	ID          int     `json:"id"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	ImgUrl      string  `json:"img_url"`
+}
+
 func welcome(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome to my ecommerce......!")
 }
@@ -13,4 +21,10 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", welcome)
+
+	fmt.Println("Server running port on:8080")
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		fmt.Println("Error from server", err)
+	}
 }
