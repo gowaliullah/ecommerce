0d24b96365f28f640b9dbffe5870431b9754b482
MD Waliullah <waliullah.godev@gmail.com>
2025-09-20 09:04:30 +0600

    Set Content-type

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ var productList []Product
 
 func getProducts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(productList)
 }
 
