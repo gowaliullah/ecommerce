3fe600d2cf2f71f35cf02aa921601d7e3f3e7608
MD Waliullah <waliullah.godev@gmail.com>
2025-09-23 11:28:12 +0600

    NewManager Middleware

--- cmd/serve.go
+++ cmd/serve.go
@@ -4,21 +4,23 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gowalillah/ecommerce/middlewares"
+	"github.com/gowalillah/ecommerce/middleware"
+	middlewares "github.com/gowalillah/ecommerce/middleware"
+	"github.com/gowalillah/ecommerce/rest/handlers"
 	"github.com/gowalillah/ecommerce/rest/handlers/products"
 )
 
 func Serve() {
 	mux := http.NewServeMux()
 
-	// mux.Handle("/products", http.HandlerFunc(getProducts))
+	mux.Handle("GET /", middlewares.Logger(http.HandlerFunc(handlers.Welcome)))
 
-	mux.Handle("GET products", http.HandlerFunc(products.GetProducts))
+	mux.Handle("GET /products", http.HandlerFunc(products.GetProducts))
 
 	mux.Handle("/create-product", http.HandlerFunc(products.CreateProduct))
 
 	fmt.Println("Server running port on:8080")
-	err := http.ListenAndServe(":8080", middlewares.GlobalRouter(mux))
+	err := http.ListenAndServe(":8080", middleware.GlobalRouter(mux))
 	if err != nil {
 		fmt.Println("Error from server", err)
 	}
--- main.go
+++ main.go
@@ -1,15 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/gowalillah/ecommerce/cmd"
 )
-
-func welcome(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome to my ecommerce......!")
-}
 
 func main() {
 	cmd.Serve()
