6c2c907b19dd4ad26eeaa0f75ae0c815e48899f8
MD Waliullah <waliullah.godev@gmail.com>
2025-09-23 12:04:47 +0600

    successfully run the manager middleware

--- cmd/serve.go
+++ cmd/serve.go
@@ -5,15 +5,19 @@ import (
 	"net/http"
 
 	"github.com/gowalillah/ecommerce/middleware"
-	middlewares "github.com/gowalillah/ecommerce/middleware"
 	"github.com/gowalillah/ecommerce/rest/handlers"
 	"github.com/gowalillah/ecommerce/rest/handlers/products"
 )
 
 func Serve() {
+
+	manager := middleware.NewManager()
+
+	m := manager.With(middleware.Logger, middleware.Logger)
+
 	mux := http.NewServeMux()
 
-	mux.Handle("GET /", middlewares.Logger(http.HandlerFunc(handlers.Welcome)))
+	mux.Handle("GET /", m(http.HandlerFunc(handlers.Welcome)))
 
 	mux.Handle("GET /products", http.HandlerFunc(products.GetProducts))
 
