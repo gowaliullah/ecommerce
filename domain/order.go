dea66461f0f32a7d5a00534d9cc8c3b30e63b653
MD Waliullah <waliullah.godev@gmail.com>
2025-09-26 19:42:08 +0600

    refactor order type

--- domain/order.go
+++ domain/order.go
@@ -2,11 +2,22 @@ package domain
 
 import "time"
 
+// OrderStatus type for ENUM mapping
+type OrderStatus string
+
+const (
+	StatusPending   OrderStatus = "pending"
+	StatusPaid      OrderStatus = "paid"
+	StatusShipped   OrderStatus = "shipped"
+	StatusDelivered OrderStatus = "delivered"
+	StatusCancelled OrderStatus = "cancelled"
+)
+
 type Order struct {
-	ID        int       `json:"id" db:"id"`
-	UserID    int       `json:"user_id" db:"user_id"`
-	Total     float64   `json:"total_price" db:"total_price"`
-	Status    string    `json:"status" db:"status"`
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	ID        int         `json:"id" db:"id"`
+	UserID    int         `json:"user_id" db:"user_id"`
+	Total     float64     `json:"total_price" db:"total_price"`
+	Status    OrderStatus `json:"status" db:"status"`
+	CreatedAt time.Time   `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time   `json:"updated_at" db:"updated_at"`
 }
