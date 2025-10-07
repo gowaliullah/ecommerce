50b130f92896dfc0a561f32aa6dcf372af50a70e
MD Waliullah <waliullah.godev@gmail.com>
2025-10-07 10:48:22 +0600

    refactor: add migration scripts for creating and dropping products table

---
+++ migrations/000002-create-products.up.sql
@@ -0,0 +1,13 @@
+-- +migrate Up
+
+CREATE TABLE IF NOT EXISTS products (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    price NUMERIC(10,2) NOT NULL,
+    stock INT DEFAULT 0,
+    img_url VARCHAR(255) NOT NULL,
+    -- category_id INT REFERENCES categories(id) ON DELETE SET NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
