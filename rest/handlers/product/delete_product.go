8a117cccb4a1ae56a62cd2db31256aa371d9c6fa
MD Waliullah <waliullah.godev@gmail.com>
2025-10-07 11:41:41 +0600

    feat: implement DeleteProduct handler for product deletion

---
+++ rest/handlers/product/delete_product.go
@@ -0,0 +1,26 @@
+package product
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gowalillah/ecommerce/util"
+)
+
+func (h *Handler) DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	productId := r.PathValue("id")
+	id, err := strconv.Atoi(productId)
+	if err != nil {
+		http.Error(w, "Please give the valid product id", 400)
+		return
+	}
+
+	err = h.svc.Delete(id)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	util.SendData(w, http.StatusOK, "Successfully deleted product")
+
+}
