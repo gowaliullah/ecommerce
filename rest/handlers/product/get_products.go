37e0243ec1880fb0b3887546c349ce9bfa29c875
MD Waliullah <waliullah.godev@gmail.com>
2025-10-07 08:20:30 +0600

    refactor: update GetProducts handler to retrieve products from service layer

--- rest/handlers/product/get_products.go
+++ rest/handlers/product/get_products.go
@@ -1,12 +1,16 @@
 package product
 
 import (
-	"encoding/json"
 	"net/http"
 
-	"github.com/gowalillah/ecommerce/database"
+	"github.com/gowalillah/ecommerce/util"
 )
 
 func (h *Handler) GetProducts(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(database.ProductList)
+	products, err := h.svc.List()
+	if err != nil {
+		util.SendError(w, http.StatusInternalServerError, "Failed to retrieve products")
+		return
+	}
+	util.SendData(w, http.StatusOK, products)
 }
