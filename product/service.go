29c805ddd637d5a1028d31859b82501594f1ecf2
MD Waliullah <waliullah.godev@gmail.com>
2025-10-07 09:41:30 +0600

    refactor: implement CRUD operations in service layer for product management

--- product/service.go
+++ product/service.go
@@ -15,3 +15,23 @@ func NewService(prdRepo ProductRepo) Service {
 func (svc *service) Create(prd domain.Product) (*domain.Product, error) {
 	return svc.prdRepo.Create(prd)
 }
+
+func (svc *service) Count() (int64, error) {
+	return svc.prdRepo.Count()
+}
+
+func (svc *service) List() ([]*domain.Product, error) {
+	return svc.prdRepo.List()
+}
+
+func (svc *service) Get(id int) (*domain.Product, error) {
+	return svc.prdRepo.Get(id)
+}
+
+func (svc *service) Update(prd domain.Product) (*domain.Product, error) {
+	return svc.prdRepo.Update(prd)
+}
+
+func (svc *service) Delete(id int) error {
+	return svc.prdRepo.Delete(id)
+}
