6e83c29c500be2e64365b5ad50aa8813af81131e
MD Waliullah <waliullah.godev@gmail.com>
2025-10-07 07:41:11 +0600

    refactor: add WrapMux method to middleware Manager for cleaner handler wrapping

--- rest/middleware/manager.go
+++ rest/middleware/manager.go
@@ -32,6 +32,14 @@ func (mngr *Manager) With(next http.Handler, middlewares ...Middleware) http.Han
 	return n
 }
 
+func (mngr *Manager) WrapMux(handler http.Handler) http.Handler {
+	h := handler
+	for _, middleware := range mngr.globalMiddlewares {
+		h = middleware(h)
+	}
+	return h
+}
+
 func (mngr *Manager) WithPrev(handlers http.Handler, middlewares ...Middleware) Middleware {
 	return func(next http.Handler) http.Handler {
 		n := next
--- rest/server.go
+++ rest/server.go
@@ -12,15 +12,26 @@ type Server struct {
 	cnf *config.Config
 }
 
+func NewServer(
+	cnf *config.Config,
+) *Server {
+	return &Server{
+		cnf: cnf,
+	}
+}
+
 func (server *Server) Start() {
 	mux := http.NewServeMux()
 	manager := middleware.NewManager()
-	manager.Use(middleware.Logger, middleware.Logger, middleware.Logger)
+	manager.Use(middleware.Logger)
+
+	// Wrap the mux with the middleware
+	wrappedMux := manager.WrapMux(mux)
 
 	// initRoutes(mux, manager)
 
 	fmt.Println("Server running port on:8080")
-	err := http.ListenAndServe(":8080", middleware.GlobalRouter(mux))
+	err := http.ListenAndServe(":8080", wrappedMux)
 	if err != nil {
 		fmt.Println("Error from server", err)
 	}
