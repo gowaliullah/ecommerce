ed1068b026f84052eb481646570d0a16c00578f3
MD Waliullah <waliullah.godev@gmail.com>
2025-10-13 10:59:35 +0600

    feat: GetCategories handler

---
+++ rest/handlers/category/get_categories.go
@@ -0,0 +1,16 @@
+package category
+
+import (
+	"net/http"
+
+	"github.com/gowalillah/ecommerce/util"
+)
+
+func (h *Handler) GetCategories(w http.ResponseWriter, r *http.Request) {
+	categories, err := h.svc.List()
+	if err != nil {
+		util.SendError(w, http.StatusInternalServerError, "Failed to retrieve categories")
+		return
+	}
+	util.SendData(w, http.StatusOK, categories)
+}
