b090e18957e9f2aac939501e35f20e968717e74e
MD Waliullah <waliullah.godev@gmail.com>
2025-10-15 17:05:25 +0600

    create repo created

--- repo/category.go
+++ repo/category.go
@@ -1,1 +1,56 @@
 package repo
+
+import (
+	"github.com/gowalillah/ecommerce/category"
+	"github.com/gowalillah/ecommerce/domain"
+	"github.com/jmoiron/sqlx"
+)
+
+
+type CategoryRepo interface {
+	category.CategoryRepo
+}
+
+type categoryRepo struct {
+	db sqlx.DB
+}
+
+
+func NewCategoryRepo(db sqlx.DB) CategoryRepo {
+	return &categoryRepo{
+		db: db,
+	}
+}
+
+
+func (r *categoryRepo) Create(c domain.Category) (*domain.Category, error) {
+	query := `
+		INSERT INTO categories (
+			name image_url
+		) VALUES (
+			$1, $2 
+		) RETURNING id
+	`
+	row := r.db.QueryRow(query, c.Name, c.ImageUrl)
+	err := row.Scan(&c.ID)
+	if err != nil {
+		return nil, err
+	}
+	
+	return &c, nil
+}
+func (r *categoryRepo)  {
+	
+}
+func (r *categoryRepo)  {
+	
+}
+func (r *categoryRepo)  {
+	
+}
+func (r *categoryRepo)  {
+	
+}
+func (r *categoryRepo)  {
+	
+}
